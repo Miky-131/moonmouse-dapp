1d45260d1b9baa4a413be1f039fd39c2112d637c
Miky-131 <cryptogame131@gmail.com>
2024-06-25 02:35:52 +1000

    u

--- src/pages/pools.tsx
+++ src/pages/pools.tsx
@@ -1157,7 +1157,7 @@ function PoolCardDatabaseBodyCollapseItemContent({ poolInfo: info }: { poolInfo:
             >
               Add Liquidity
             </Button>
-            <Tooltip>
+            {/* <Tooltip>
               <Icon
                 size="smi"
                 iconSrc="/icons/pools-farm-entry.svg"
@@ -1175,7 +1175,7 @@ function PoolCardDatabaseBodyCollapseItemContent({ poolInfo: info }: { poolInfo:
                 }}
               />
               <Tooltip.Panel>Farm</Tooltip.Panel>
-            </Tooltip>
+            </Tooltip> */}
             <Tooltip>
               <Icon
                 size="smi"
